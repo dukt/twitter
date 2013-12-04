9831bbacae4d8085c881cc6e219adfc7ed2078f6
Benjamin David <ben@dukt.net>
2013-12-05 00:00:20 +0100

    0.9.2

--- twitter/TwitterPlugin.php
+++ twitter/TwitterPlugin.php
@@ -29,7 +29,7 @@ class TwitterPlugin extends BasePlugin
      */
     function getVersion()
     {
-        return '0.9.1';
+        return '0.9.2';
     }
 
     // --------------------------------------------------------------------
