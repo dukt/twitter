20a0c6790fe2520b8eb2d76925f37b4c77b048bb
Benjamin David <ben@dukt.net>
2015-05-10 12:15:29 +0200

    Plugin requires OAuth 0.9.69+

--- Source/twitter/TwitterPlugin.php
+++ Source/twitter/TwitterPlugin.php
@@ -37,7 +37,7 @@ class TwitterPlugin extends BasePlugin
                 'name' => "OAuth",
                 'handle' => 'oauth',
                 'url' => 'https://dukt.net/craft/oauth',
-                'version' => '0.9.68'
+                'version' => '0.9.69'
             )
         );
     }
