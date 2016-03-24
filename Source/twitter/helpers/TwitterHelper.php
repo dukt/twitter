72e02db21f7a31af033f0d3465e018eacda0821e
Benjamin David <ben@dukt.net>
2016-03-24 20:57:13 +0100

    Improved time ago calculation

--- Source/twitter/helpers/TwitterHelper.php
+++ Source/twitter/helpers/TwitterHelper.php
@@ -44,8 +44,81 @@ class TwitterHelper
 
 		$difference = $now->getTimestamp() - $date->getTimestamp();
 
-		$duration = DateTimeHelper::secondsToHumanTimeDuration($difference);
+		$durations = self::secondsToHumanTimeDuration($difference, true, false);
+
+		$duration = Craft::t("{duration} ago", ['duration' => $durations[0]]);
 
 		return $duration;
+	}
+
+	/**
+	 * @param int  $seconds     The number of seconds
+	 * @param bool $showSeconds Whether to output seconds or not
+	 * @param bool $implodeComponents Whether to implode components or not for return
+	 *
+	 * @return string|array
+	 */
+	public static function secondsToHumanTimeDuration($seconds, $showSeconds = true, $implodeComponents = true)
+	{
+		$secondsInWeek   = 604800;
+		$secondsInDay    = 86400;
+		$secondsInHour   = 3600;
+		$secondsInMinute = 60;
+
+		$weeks = floor($seconds / $secondsInWeek);
+		$seconds = $seconds % $secondsInWeek;
+
+		$days = floor($seconds / $secondsInDay);
+		$seconds = $seconds % $secondsInDay;
+
+		$hours = floor($seconds / $secondsInHour);
+		$seconds = $seconds % $secondsInHour;
+
+		if ($showSeconds)
+		{
+			$minutes = floor($seconds / $secondsInMinute);
+			$seconds = $seconds % $secondsInMinute;
+		}
+		else
+		{
+			$minutes = round($seconds / $secondsInMinute);
+			$seconds = 0;
+		}
+
+		$timeComponents = array();
+
+		if ($weeks)
+		{
+			$timeComponents[] = $weeks.' '.($weeks == 1 ? Craft::t('week') : Craft::t('weeks'));
+		}
+
+		if ($days)
+		{
+			$timeComponents[] = $days.' '.($days == 1 ? Craft::t('day') : Craft::t('days'));
+		}
+
+		if ($hours)
+		{
+			$timeComponents[] = $hours.' '.($hours == 1 ? Craft::t('hour') : Craft::t('hours'));
+		}
+
+		if ($minutes || (!$showSeconds && !$weeks && !$days && !$hours))
+		{
+			$timeComponents[] = $minutes.' '.($minutes == 1 ? Craft::t('minute') : Craft::t('minutes'));
+		}
+
+		if ($seconds || ($showSeconds && !$weeks && !$days && !$hours && !$minutes))
+		{
+			$timeComponents[] = $seconds.' '.($seconds == 1 ? Craft::t('second') : Craft::t('seconds'));
+		}
+
+		if($implodeComponents)
+		{
+			return implode(', ', $timeComponents);
+		}
+		else
+		{
+			return $timeComponents;
+		}
 	}
 }
