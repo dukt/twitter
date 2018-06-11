3e3d923c22e9050c3dac1d9a4b1cd5be187f7c36
Benjamin David <ben@dukt.net>
2018-06-11 14:37:27 +0200

    Configuration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = {
                         'installation',
                         'updating',
                         'connect-twitter',
+                        'configuration',
                     ]
                 },
                 {
