5f531eb52559aca1ea3ff90f17ce6971d0c84d3d
Benjamin David <ben@dukt.net>
2021-03-09 19:08:35 +0100

    2021

--- src/services/Publish.php
+++ src/services/Publish.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link      https://dukt.net/twitter/
- * @copyright Copyright (c) 2019, Dukt
+ * @copyright Copyright (c) 2021, Dukt
  * @license   https://github.com/dukt/twitter/blob/master/LICENSE.md
  */
 
